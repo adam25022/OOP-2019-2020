78244ac810cb8f54f4b8fbd0ec6e01ee8a61e3c8
adam25022 <adamnol2502@gmail.com>
2020-02-19 15:51:43 +0000

    text

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,6 +4,10 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet
 {	
+	//float[] rainfall = new float[12];
+	float[] rainfall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58};
+	String[] months = {"an", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Oct", "Nov", "Dec"};
+	
 	public void settings()
 	{
 		size(500, 500);
@@ -11,7 +15,21 @@ public class Arrays extends PApplet
 
 	public void setup() 
 	{
-		
+		for(int i=0;i<rainfall.length;i++)
+		{
+			println(rainfall[i] + "/t" + rainfall[i]);
+		}
+
+		for(float f:rainfall)
+		{
+			println(f);
+		}
+
+		for(String s:months)
+		{
+			println(s);
+		}
+
 	}
 
 	
