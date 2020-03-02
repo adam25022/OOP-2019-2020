6bd27d2c6e735bc789ae3b5fc44555113e7370e5
adam25022 <adamnol2502@gmail.com>
2020-03-02 12:26:22 +0000

    Commit

--- java/src/ie/tudublin/Sound2.java
+++ java/src/ie/tudublin/Sound2.java
@@ -105,6 +105,9 @@ public class Sound2 extends PApplet
 		for(int i = 0 ; i < fft.specSize() ; i ++)
 		{
 			line(i, 0, i, fft.getBand(i) * 100);
+			
+			if(fft.getBand(i) > fft.getBand(highestBin))
+			highestBin=i;
 		}
 
 		float freq1 = fft.indexToFreq(highestBin);
